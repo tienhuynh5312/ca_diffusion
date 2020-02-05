0ab61ad2afb1386e45fe1298b47b902b48015ffd
TIEN HUYNH <tien.huynh5312@gmail.com>
2020-02-04 16:33:47 -0800

    Update animDiffusionGray.py

--- animDiffusionGray.py
+++ animDiffusionGray.py
@@ -1,2 +1,12 @@
-def animDiffusionGray():
+def animDiffusionGray(grids):
+    data = grids[0]
+    img = ax.imshow(data, interpolation='none', cmap=plt.cm.rainbow,
+                    extent=[0, 28, 0, 20],
+                    aspect='auto',
+                    zorder=0)
+    for it in range(len(grids)):
+        data = grids[it]
+        img.set_data(data)
+        plt.draw()
+        plt.pause(0.05)
     return 0
