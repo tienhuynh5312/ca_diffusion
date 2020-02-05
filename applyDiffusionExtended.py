5acea792838fdf5b411ac5e659a608d1ee58aa8b
TIEN HUYNH <tien.huynh5312@gmail.com>
2020-02-04 21:20:42 -0800

    Update

--- applyDiffusionExtended.py
+++ applyDiffusionExtended.py
@@ -1,13 +1,14 @@
 import numpy as np
 
-def diffusion(diffusionRate, site, N, NE, E, SE, S, SW, W, NW):
-    return(1-8*diffusionRate)*site + diffusionRate*(N + NE + E +  SE + S + SW + W + NW)
+from Diffusion import diffusion
 #Returns the lattice with extended boundary removed,
 # and applys diffusion to all cells
 def applyDiffusionExtended(diffusionRate, latExt): 
-    diffused = np.zeros((np.shape(latExt)[0] - 2, np.shape(latExt)[1] - 2))
-    for i in range(1, len(latExt) - 1): 
-        for j in range(1, len(latExt[0]) - 1): 
+    row_num = np.shape(latExt)[0]
+    col_num = np.shape(latExt)[1] 
+    diffused = np.zeros((row_num - 2, col_num - 2))
+    for i in range(1, row_num - 1): 
+        for j in range(1, col_num - 1): 
             site = latExt[i][j]
             N = latExt[i - 1][j]
             NE = latExt[i - 1][j + 1]
@@ -17,5 +18,5 @@ def applyDiffusionExtended(diffusionRate, latExt):
             SW = latExt[i + 1][j - 1]
             W = latExt[i][j - 1]
             NW = latExt[i - 1][j - 1]
-            diffused[i - 1][j - 1] = diffusion(latExt[i][j], site, N, NE, SE, S, SW, W, NW)
+            diffused[i - 1][j - 1] = diffusion(latExt[i][j], site, N, NE, E, SE, S, SW, W, NW)
     return diffused 
