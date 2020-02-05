cab7413bebf8e966f86cb7bc65167edd1263b48c
TIEN HUYNH <tien.huynh5312@gmail.com>
2020-02-04 16:41:07 -0800

    Update animDiffusionGray.py

--- animDiffusionGray.py
+++ animDiffusionGray.py
@@ -1,12 +1,19 @@
 def animDiffusionGray(grids):
+    import matplotlib.pyplot as plt
+    
+    fig1 = plt.figure(figsize=(10,10))
+    ax = fig1.add_axes((0, 0, 0.5, 0.5))
+    ax.axis('off')
     data = grids[0]
-    img = ax.imshow(data, interpolation='none', cmap=plt.cm.rainbow,
+    img = ax.imshow(data, interpolation='gaussian', cmap=plt.cm.rainbow,
                     extent=[0, 28, 0, 20],
                     aspect='auto',
                     zorder=0)
-    for it in range(len(grids)):
+    for it in range(1,len(grids)):
         data = grids[it]
         img.set_data(data)
         plt.draw()
-        plt.pause(0.05)
-    return 0
+        plt.pause(0.5)
+        
+    plt.show()
+        
