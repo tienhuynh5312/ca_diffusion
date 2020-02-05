af7b3e29ebb3db53d6aa7cf7422271a605abf4e9
Matthew Taing <taingmat@uw.edu>
2020-02-04 16:50:50 -0800

    fixed coordinate system

--- applyDiffusionExtended.py
+++ applyDiffusionExtended.py
@@ -5,16 +5,16 @@ import Diffusion.py as d
 # and applys diffusion to all cells
 def applyDiffusionExtended(latExt, diffusionRate): 
     diffused = np.zeros(len(latExt) - 2, len(latExt[0]) - 2)
-    for i in range(0, len(latExt) - 1): 
-        for j in range(0, len(latExt[0]) - 1): 
+    for i in range(1, len(latExt) - 1): 
+        for j in range(1, len(latExt[0]) - 1): 
             site = latExt[i][j]
-            N = latExt[i][j + 1]
-            NE = latExt[i + 1][j + 1]
-            E = latExt[i + 1][j]
-            SE = latExt[i + 1][j - 1]
-            S = latExt[i][j - 1]
-            SW = latExt[i - 1][j - 1]
-            W = latExt[i - 1][j]
-            NW = latExt[i - 1][j + 1]
+            N = latExt[i - 1][j]
+            NE = latExt[i - 1][j + 1]
+            E = latExt[i][j + 1]
+            SE = latExt[i + 1][j + 1]
+            S = latExt[i + 1][j]
+            SW = latExt[i + 1][j - 1]
+            W = latExt[i][j - 1]
+            NW = latExt[i - 1][j - 1]
             diffused[i - 1][j - 1] = d.diffusion(latExt[i][j], site, N, NE, SE, S, SW, W, NW)
     return diffused 
