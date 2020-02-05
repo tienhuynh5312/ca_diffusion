635af035f05473d4e5859c55e3d66ce5680b4a76
Matthew Taing <taingmat@uw.edu>
2020-02-04 16:16:46 -0800

    v1 of applyDiffusionExtended

--- applyDiffusionExtended.py
+++ applyDiffusionExtended.py
@@ -7,13 +7,14 @@ def applyDiffusionExtended(latExt, diffusionRate):
     diffused = np.zeros(len(latExt) - 2, len(latExt[0:]) - 2)
     for i in range(0, len(latExt) - 1): 
         for j in range(0, len(latExt[0]) - 1): 
-            site = diffused[i][j]
+            site = latExt[i][j]
             N = latExt[i][j + 1]
             NE = latExt[i + 1][j + 1]
-            E = latExt[i][j]
-            SE = latExt[i][j]
-            S = latExt[i][j]
-            SW = latExt[i][j]
-            W = latExt[i][j]
-            NW = latExt[i][j]
+            E = latExt[i + 1][j]
+            SE = latExt[i + 1][j - 1]
+            S = latExt[i][j - 1]
+            SW = latExt[i - 1][j - 1]
+            W = latExt[i - 1][j]
+            NW = latExt[i - 1][j + 1]
             diffused[i - 1][j - 1] = d.diffusion(latExt[i][j], site, N, NE, SE, S, SW, W, NW)
+    return diffused 
