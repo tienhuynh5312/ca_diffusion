a2cf55f4d4babaa50414a964c2da2d4fe2b68a2f
TIEN HUYNH <tien.huynh5312@gmail.com>
2020-02-04 16:11:09 -0800

    Update initBar.py

--- initBar.py
+++ initBar.py
@@ -18,3 +18,14 @@ def applyHotCold(site, hotSites, coldSites):
         site[i[0],i[1]] = COLD
     
     return site
+
+def diffusionSim(m,n, diffusionRate, t):
+    bar = initBar(m,n, hotSites, coldSites)
+    grids = [bar]
+    for i in range(t):
+        barExtended = reflectinglat(bar)
+        bar = applyDiffusionExtended(diffusionRate, barExtended)
+        bar = applyHotCold(bar, hotSites, coldSites)
+        grids.append(bar)
+        
+    return grids
